af70ec1a9e49403f99d2e468ebd4e320b2fbb3e8
Eleonore Mayola (Eleonore9) <eleonore.mayola@ymail.com>
2017-02-22 19:12:14 +0000

    Require lapita/plot to test barchart

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [kixi.lapita "0.1.0-SNAPSHOT"]
                  [prismatic/schema "1.1.3"]
                  [schema-contrib "0.1.3"]
-                 [instaparse "1.4.3"]])
+                 [instaparse "1.4.3"]]
+  :profiles {:user {:dependencies [[kixi.lapita "0.1.0-SNAPSHOT"]]}})
--- src/witan/reparaturen.clj
+++ src/witan/reparaturen.clj
@@ -1,7 +1,8 @@
 (ns witan.reparaturen
   (:require [schema.core :as s]
             [schema-contrib.core :as c]
-            [kixi.lapita :as kl]))
+            [kixi.lapita :as kl]
+            [kixi.lapita.plot :as plot]))
 
 (def HousingRepairSchema
   {:repair-number s/Str
@@ -14,6 +15,11 @@
    :workforce-name s/Str
    :description-for-code s/Str})
 
+(def TestData
+  {:col-1 s/Int
+   :col-2 s/Str
+   :col-3 java.lang.Double})
+
 (comment (def repairs-data (kl/csv-to-maps-coll "data/historic-repairs-2014-2015.csv"))
 
          (def repairs-ds (kl/csv-to-dataset "data/historic-repairs-2014-2015.csv"))
@@ -21,4 +27,19 @@
          (def repairs-ds-cleaned (kl/csv-to-dataset "data/historic-repairs-2014-2015.csv"
                                                     HousingRepairSchema
                                                     {:print-errors true
-                                                     :remove-errors true})))
+                                                     :remove-errors true}))
+
+         (-> repairs-ds-cleaned
+             (kl/count-elements-in-column :property-reference :count-repairs-per-property)
+             (kl/count-elements-in-column :count-repairs-per-property :occurences)
+             (plot/plot-bar-chart :count-repairs-per-property :occurences
+                                  "data/occurences-of-n-repairs.svg"
+                                  {:plot-width 900 :plot-height 500}))
+
+         (def test-data-1 [{:col-1 "1" :col-2 "a" :col-3 "1.1"}
+                           {:col-1 "2" :col-2 "b" :col-3 "bar"}
+                           {:col-1 "foo" :col-2 "c" :col-3 "1.3"}])
+
+         (def test-data-2 [{:col-1 "1" :col-2 "a" :col-3 "1.1"}
+                           {:col-1 "2" :col-2 "b" :col-3 "1.2"}
+                           {:col-1 "3" :col-2 "c" :col-3 "1.3"}]))
